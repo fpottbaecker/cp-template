e479cb30a835ee1098e6b12e900a99ccba6c3e16
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-05-21 23:59:31 +0200

    Add compiler definitions

--- templates/contest.cmake
+++ templates/contest.cmake
@@ -1,9 +1,7 @@
 if(!PROJECT_NAME)
 	cmake_minimum_required(VERSION 3.1 FATAL_ERROR)
 	project(%REPO_NAME%-%CONTEST_NAME%)
-
-	set(CMAKE_CXX_STANDARD 17)
-	include_directories(${CMAKE_CURRENT_LIST_DIR}/../../include)
+	include(${CMAKE_CURRENT_LIST_DIR}/../../common.cmake)
 endif()
 
 include(contents.cmake)
