8db5f3bb0333b86e06a03d4653879a4ad32981f1
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-05-21 23:34:59 +0200

    Finalize template generation

---
+++ scripts/load_tasks.cmake
@@ -0,0 +1,17 @@
+
+set(CONTEST_DIR ${CMAKE_CURRENT_SOURCE_DIR})
+
+# find samples
+file(GLOB TASK_SAMPLES RELATIVE ${CONTEST_DIR} samples-*.zip)
+
+# extract samples
+foreach(TASK_SAMPLE IN LISTS TASK_SAMPLES)
+    string(REPLACE samples- "" TASK_NAME ${TASK_SAMPLE})
+    string(REPLACE .zip "" TASK_NAME ${TASK_NAME})
+    message("Loaded task ${TASK_NAME}.")
+    include(${CMAKE_CURRENT_LIST_DIR}/add_task.cmake)
+    make_directory(${CONTEST_DIR}/${TASK_NAME}/samples)
+    execute_process(COMMAND ${CMAKE_COMMAND} -E tar x ${CONTEST_DIR}/${TASK_SAMPLE}
+            WORKING_DIRECTORY ${CONTEST_DIR}/${TASK_NAME}/samples)
+    file(REMOVE ${CONTEST_DIR}/${TASK_SAMPLE})
+endforeach()
--- templates/contest.cmake
+++ templates/contest.cmake
@@ -1,6 +1,9 @@
 if(!PROJECT_NAME)
 	cmake_minimum_required(VERSION 3.1 FATAL_ERROR)
-	project(cp2020-%CONTEST_NAME%)
+	project(%REPO_NAME%-%CONTEST_NAME%)
+
+	set(CMAKE_CXX_STANDARD 17)
+	include_directories(${CMAKE_CURRENT_LIST_DIR}/../../include)
 endif()
 
 include(contents.cmake)
