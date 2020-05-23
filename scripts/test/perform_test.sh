6ba1b9c7d353728ab086da69d9d68a25456ec7b0
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-05-24 00:38:48 +0200

    Document test runner

--- scripts/test/perform_test.sh
+++ scripts/test/perform_test.sh
@@ -17,13 +17,14 @@ rm -f "$RESULT_OUTPUT" "$RESULT_DIFF" "$RESULT_ERROR"
 # Run target
 $EXECUTABLE_FILE <"$SAMPLE_INPUT" >"$RESULT_OUTPUT" 2>"$RESULT_ERROR"
 
-# Commit suicide if target failed (SIGSEGV)
+# Commit suicide if target failed to report exception for CTest (SIGSEGV)
 if [ $? -ne 0 ]; then
   echo "Run Error"
   rm "$RESULT_OUTPUT"
   kill -11 $$
 fi
 
+# Remove error output if empty
 if [ ! -s $RESULT_ERROR ]; then
   rm "$RESULT_ERROR"
 fi
@@ -38,6 +39,7 @@ if [ $? -eq 0 ]; then
   rm "$RESULT_DIFF"
   exit 0
 else
+  # Output truncated diff
   MAX_LINES=12
   DIFF_LINES=$(($(wc -l <"$RESULT_DIFF")))
   head -n $MAX_LINES "$RESULT_DIFF"
