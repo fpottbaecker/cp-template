5b9c4146b2c09eb0dc086f2ebc2b04d7cf366da3
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-05-24 01:06:38 +0200

    Enhance windows support

--- scripts/test/perform_test.cmake
+++ scripts/test/perform_test.cmake
@@ -2,6 +2,9 @@
 # Arg: SAMPLE_ROOT
 # Arg: SAMPLE
 
+# TODO: This is probably not platform independent
+set(DIFF_COMMAND diff -byd -W 80)
+
 set(SAMPLE_INPUT ${SAMPLE_ROOT}/${SAMPLE}.in)
 set(SAMPLE_OUTPUT ${SAMPLE_ROOT}/${SAMPLE}.out)
 set(RESULT_OUTPUT ${SAMPLE_ROOT}/${SAMPLE}.result)
@@ -21,22 +24,25 @@ execute_process(COMMAND ${EXECUTABLE_FILE}
 if (NOT EXECUTION_RESULT EQUAL "0")
     file(WRITE ${RESULT_ERROR} "EXIT CODE: ${EXECUTION_RESULT}\n\n")
     file(APPEND ${RESULT_ERROR} ${EXECUTION_ERROR})
-    message("Run Error")
-    execute_process(COMMAND ${CMAKE_CURRENT_LIST_DIR}/patricide.sh)
+    message(SEND_ERROR "Run Error")
 endif()
 
-execute_process(COMMAND diff -Bbyd ${RESULT_OUTPUT} ${SAMPLE_OUTPUT}
+execute_process(COMMAND ${DIFF_COMMAND} ${RESULT_OUTPUT} ${SAMPLE_OUTPUT}
         OUTPUT_VARIABLE DIFF_OUTPUT
         RESULT_VARIABLE DIFF_RESULT)
 
 if (NOT DIFF_RESULT EQUAL "0")
-    file(WRITE ${RESULT_DIFF} ${DIFF_OUTPUT})
-    message(SEND_ERROR "Sample ${SAMPLE}: Wrong Answer")
+    file(WRITE ${RESULT_DIFF}
+            "Wrong Answer                            Correct Answer\n"
+            "------------------------------------------------------\n")
+    file(APPEND ${RESULT_DIFF} ${DIFF_OUTPUT})
+    message(SEND_ERROR "")
     string(LENGTH ${DIFF_OUTPUT} DIFF_LENGTH)
     if (DIFF_LENGTH LESS "1000")
+        message("Wrong Answer                            Correct Answer")
+        message("------------------------------------------------------")
         message(${DIFF_OUTPUT})
     else()
         message("See ${RESULT_DIFF}")
     endif()
 endif()
-
