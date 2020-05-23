4543d0c54f8ed276a45bce419ba33add0e94f71e
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-05-24 01:25:19 +0200

    Add reduced template

---
+++ templates/template.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <string>
+#include <functional>
+// Not standard, so uncomment at your own peril
+//#include <bits/stdc++.h>
+
+using namespace std;
+
+#define rep(a, b)   for(int a = 0; a < (b); ++a)
+#define all(v) (v).begin(), (v).end()
+#define range(a, n) (a), ((a) + (n))
+
+template <typename cT, typename Traits>
+std::basic_ostream<cT, Traits>&
+nl(std::basic_ostream<cT, Traits>& out)
+{
+    return out << out.widen('\n');
+}
+
+int main(int argc, char** argv) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+
+
+    return 0;
+}
