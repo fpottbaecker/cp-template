4c3dc0c13bc46f3cebbd9abd14f01b41b0dbce1a
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2020-05-24 13:59:40 +0200

    Add builds task

--- contests/test/builds/builds.cpp
+++ contests/test/builds/builds.cpp
@@ -25,7 +25,24 @@ int main(int argc, char** argv) {
     cin.tie(nullptr);
     cout.precision(10);
 
+    string op;
+    cin >> op;
 
+    if (op == "finish") {
+        cout << 0 << nl;
+        cout << 0 << nl;
+    } else if (op == "error") {
+        return 1;
+    } else if (op == "fault") {
+        vector<char> cs(100, 'a');
+        for (unsigned int i = 0; i <= 100; ++i) {
+            cout << cs.at(i) << nl;
+        }
+    } else if (op == "infinity") {
+        for (int i = 0; i < 4'000'000'000; ++i) {
+            cout << 0 << nl;
+        }
+    }
 
     return 0;
 }
